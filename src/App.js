a61bb35e12f70a2d7787ea383ada8c3dd40209f4
issabayevmk <issabayevmk@gmail.com>
2022-12-28 14:38:00 +0100

    actions test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World! It is main file! Uhhu!
+          Hello World! It is main file!
         </p>
         <a
           className="App-link"
